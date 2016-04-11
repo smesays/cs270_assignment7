4fbe0da8daf81ffa266e993db71f12d856053c2e
smesays <g.liu@smesays.com>
2016-04-11 20:59:38 +0000

    Created new and update pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,20 @@ class EventsController < ApplicationController
     def new_event
     end
     
+    # POST /events/create
+    def create_event
+        @event = Event.new( name:       params[:name], 
+                            description:params[:description],
+                            start_date: params[:start_date],
+                            end_date:   params[:end_date]
+                          )
+        if @event.save
+            redirect_to show_event_path(@event)
+        else
+            redirect_to root_path
+        end
+    end
+    
     # GET /events
     def show_all_events
         @events = Event.all
@@ -15,6 +29,21 @@ class EventsController < ApplicationController
     
     # GET /events/:id/edit
     def edit_event
+        @event = Event.find(params[:id])
+    end
+    
+    # POST /events/:id/update
+    def update_event
+        @event = Event.find(params[:id])
+        
+        if @event.update(   name:       params[:event][:name],
+                            description:params[:event][:description],
+                            coordinates:params[:event][:coordinates]
+                        )
+            redirect_to show_event_path(@event)
+        else
+            redirect_to root_path
+        end
     end
     
     # DELETE /events/:id
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,16 +3,46 @@ class LocationsController < ApplicationController
     def new_location
     end
     
+    # POST /locations/create
+    def create_location
+        @location = Location.new(   name:           params[:name], 
+                                    description:    params[:description],
+                                    coordinates:    params[:coordinates]
+                                )
+        if @location.save
+            redirect_to show_location_path(@location)
+        else
+            redirect_to root_path
+        end
+    end
+    
     # GET /locations
     def show_all_locations
+        @locations = Location.all
     end
     
     # GET /locations/:id
     def show_location
+        @location = Location.find(params[:id])
     end
     
     # GET /locations/:id/edit
     def edit_location
+         @location = Location.find(params[:id])
+    end
+
+    # POST /locations/:id/update
+    def update_location
+        @location = Location.find(params[:id])
+        
+        if @location.update(name:           params[:event][:name],
+                            description:    params[:event][:description],
+                            coordinates:    params[:event][:coordinates]
+                           )
+            redirect_to show_location_path(@location)
+        else
+            redirect_to root_path
+        end
     end
     
     # DELETE /locations/:id
